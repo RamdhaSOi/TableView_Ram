d128cb52f8810ca9e76801dca8ab5a5a53da7689
Ramdhas <ramdhasios@gmail.com>
2014-06-17 11:10:27 +0530

    Initial Commit

---
+++ TableView_Ram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableView_Ram
+//
+//  Created by Ramdhas on Jun,17.
+//  Copyright (c) 2014 Ramdhas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableView_Ram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableView_Ram
+//
+//  Created by Ramdhas on Jun,17.
+//  Copyright (c) 2014 Ramdhas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
